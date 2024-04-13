8a48b10cdf2a92d10a791cce3b14a6b2d2da81b5
Aswad Tariq <aswadtariq05@gmail.com>
2024-04-13 17:21:06 -0300

    Keystroke attack

--- source_code/main.py
+++ source_code/main.py
@@ -80,27 +80,38 @@ def main():
         os.makedirs(data_metric_save_path)
     # hmog_out = os.path.join(root_path, 'processed_data\\hmog_touch\\df_10.csv')
     # df = hmog_parser.HMOGParser().get_feature_vectors(hmog_out, limit=2)
+    root_path = Path(__file__).parent.parent
+    data_path = os.path.join(root_path, "processed_data\\dsn_keystroke")
+    print(data_path)
+    feature_paths = {f'gr{gr}': os.path.join(data_path, f'df_group_{gr}.csv') for gr in range(1, 3)}
+    data = {f'{gr}': pd.read_csv(path) for gr, path in feature_paths.items()}
+    tb_data_group_1 = BioDataSet(feature_data_frame=data['gr1'], random_state=0)
+    users_group_1 = tb_data_group_1.user_list
+    data_group_1 = dict()
+    for user in users_group_1:
+            data_group_1[user] = tb_data_group_1.get_data_set(user, neg_sample_sources=None, neg_test_limit=True)
+    
     Data = preprocess_hmog_touch_data(root_path=root_path)
-    pos_user = 240168
+    pos_user = 's002'
     mn = MonacoNormalize()
-    df = pd.DataFrame(Data[pos_user])
+    data_frame= pd.DataFrame(data_group_1[pos_user])
     # # print(Data)
-    data_frame = mn.operate(pos_user, df)
+    # data_frame = mn.operate(pos_user, df)
     data_frame.to_csv(os.path.join(data_metric_save_path, "user_pop_df.csv"), index=False, mode='w+')
     # print(data_frame.to_string())
-    nd = Data[pos_user].head(50)
-    nd = nd.append(Data[100669])
-    nd['labels'] = np.where(nd['user'] == pos_user, 1, nd['labels'])
-    nd['labels'] = np.where(nd['user'] != pos_user, 0, nd['labels'])
+    # nd = Data[pos_user].head(50)
+    # nd = nd.append(Data[100669])
+    # nd['labels'] = np.where(nd['user'] == pos_user, 1, nd['labels'])
+    # nd['labels'] = np.where(nd['user'] != pos_user, 0, nd['labels'])
     
     feature_names = list(data_frame.columns.drop(['user', 'labels']))
 
-    for feat in feature_names:
-        lb = mn.bounds.loc[feat, 'lower_bound']
-        ub = mn.bounds.loc[feat, 'upper_bound']
-        nd[feat] = mn.monormalize(feature=nd[feat], lower_bound=lb, upper_bound=ub)
+    # for feat in feature_names:
+    #     lb = mn.bounds.loc[feat, 'lower_bound']
+    #     ub = mn.bounds.loc[feat, 'upper_bound']
+    #     nd[feat] = mn.monormalize(feature=nd[feat], lower_bound=lb, upper_bound=ub)
 
-    clf_knn = KnnClassifier(pos_user=pos_user, random_state=0)
+    clf_knn = KnnClassifier(n_neighbors=20, pos_user=pos_user, random_state=0)
     clf_knn.split_data(data_frame=data_frame, training_data_size=0.6, save_path=data_metric_save_path)
 
     leaf_size = list(range(1, 70))
@@ -118,11 +129,12 @@ def main():
     knn_important_features_perm_ = knn_important_features_perm_.sort_values(by=0)
 
     predictions_knn = clf_knn.classify()
+    print(len(predictions_knn))
     print(predictions_knn)
 
     test_set_knn = clf_knn.test_data_frame.drop('labels', axis=1)
     test_labels_knn = clf_knn.test_data_frame.labels.values
-
+    print(test_labels_knn)
     knn_cm_path = os.path.join(data_metric_save_path, 'knn_cm.csv')
     cm_knn = ConfusionMatrix()
     matrix_knn = cm_knn.get_metric(true_labels=test_labels_knn, predicted_labels=predictions_knn, output_path=knn_cm_path)
@@ -151,6 +163,9 @@ def main():
                     pred_labels=clf_knn.predictions)
     plt.savefig(os.path.join(data_metric_save_path, 'FCS_KNN.png'))
 
+# def main_2():
+    
+
 
 
 def split_touch_data_into_groups():
@@ -176,8 +191,8 @@ def split_touch_data_into_groups():
     df_last_50.to_csv('df_group_2.csv', index=False)
 
 def split_keyboard_data_into_groups():
-    raw_data_path = 'D:\\Waterloo Work\\SecurityMetrics\\raw_data\\dsn_keystroke\\DSL-StrongPasswordData.csv'
-    output_path = 'D:\\Waterloo Work\\SecurityMetrics\\processed_data\\dsn_keystroke\\df'
+    raw_data_path = 'D:\\Waterloo Work\\CS 848 Privacy Preserving Datasystems\\Project\\SecurityMetrics\\raw_data\\dsn_keystroke\\DSL-StrongPasswordData.csv'
+    output_path = 'D:\\Waterloo Work\\CS 848 Privacy Preserving Datasystems\\Project\\SecurityMetrics\\processed_data\\dsn_keystroke\\df'
     df = dsn_keystroke_parser.DSNParser().raw_to_feature_vectors(raw_data_path, output_path)
 
     unique_users = df['user'].unique()
@@ -187,6 +202,7 @@ def split_keyboard_data_into_groups():
     print(split_index)
     first_half_users = unique_users[:split_index]
     second_half_users = unique_users[split_index:]
+    print(first_half_users['user' == 's002'])
 
     # Split the DataFrame based on the user IDs
     df_first_half = df[df['user'].isin(first_half_users)]
@@ -199,8 +215,10 @@ def split_keyboard_data_into_groups():
     df_second_half.to_csv('second_half_users.csv', index=False)
 
 
+
+
 if __name__ == "__main__":
-    split_keyboard_data_into_groups()
+    main()
     pass
 
 
